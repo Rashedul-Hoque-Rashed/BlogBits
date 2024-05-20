8fec3c5333d85bc7bf91567875f857314c6611af
Rashed <rashedulhoqueceo@gmail.com>
2024-05-20 23:24:00 +0600

    create navbar and footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
-  return <div>
-    <Link href="/">Hello</Link>
-  </div>;
+  return (
+    <div>
+      
+    </div>
+  );
 }
