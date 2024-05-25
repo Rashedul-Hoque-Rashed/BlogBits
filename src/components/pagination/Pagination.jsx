0aeee9a18cc800f928cafcbe2b0aebc7eb368d88
Rashed <rashedulhoqueceo@gmail.com>
2024-05-25 20:41:29 +0600

    create paginations

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import styles from './pagination.module.css';
+"use client";
+
+import React from "react";
+import styles from "./pagination.module.css";
+import { useRouter } from "next/navigation";
 
 const Pagination = () => {
-    return (
-        <div className={styles.container}>
-            pagination
-        </div>
-    );
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <button
+        className={styles.button}
+      >
+        Previous
+      </button>
+      <button
+        className={styles.button}
+      >
+        Next
+      </button>
+    </div>
+  );
 };
 
 export default Pagination;
