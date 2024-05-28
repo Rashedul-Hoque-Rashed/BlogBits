bf91e305621daa022c73ee8fa2336801bf9047ff
Rashed <rashedulhoqueceo@gmail.com>
2024-05-28 22:14:22 +0600

    create login action

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -2,14 +2,14 @@
 import { useState } from 'react';
 import styles from './authLinks.module.css';
 import Link from 'next/link';
-import {signOut} from 'next-auth/react';
+import {signOut, useSession} from 'next-auth/react';
 
 const AuthLinks = () => {
     const [open, setOpen] = useState(false);
-    const status = "authenticated"
+    const {status} = useSession();
     return (
         <>
-      {status === "notauthenticated" ? (
+      {status === "unauthenticated" ? (
         <Link href="/login" className={styles.link}>
           Login
         </Link>
@@ -18,7 +18,7 @@ const AuthLinks = () => {
           <Link href="/write" className={styles.link}>
             Write
           </Link>
-          <span className={styles.link} onclick={signOut}>
+          <span className={styles.link} onClick={signOut}>
             Logout
           </span>
         </>
@@ -33,12 +33,12 @@ const AuthLinks = () => {
           <Link href="/">Homepage</Link>
           <Link href="/">About</Link>
           <Link href="/">Contact</Link>
-          {status === "notauthenticated" ? (
+          {status === "unauthenticated" ? (
             <Link href="/login">Login</Link>
           ) : (
             <>
               <Link href="/write">Write</Link>
-              <span className={styles.link} onclick={signOut}>Logout</span>
+              <span onClick={signOut}>Logout</span>
             </>
           )}
         </div>
